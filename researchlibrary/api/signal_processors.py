837cc0aabd404689bb37f6673e06a85de72d286b
Denis Drescher <drescher@ferret-go.com>
2017-01-17 09:09:39 +0100

    Fix updating of inexistant indices

--- researchlibrary/api/signal_processors.py
+++ researchlibrary/api/signal_processors.py
@@ -1,23 +1,29 @@
+import logging
 from itertools import chain
 from django.db.models import signals
 from haystack.signals import BaseSignalProcessor
 from .models import Resource, Category, Keyword, Person
 
 
+logger = logging.getLogger(__name__)
+
+
 class SignalProcessor(BaseSignalProcessor):
 
     def setup(self):
-        for model in (Resource, Category, Keyword, Person):
+        for model in (Resource, Keyword, Person):  # All models with indices
             signals.post_save.connect(self.handle_save, sender=model)
             signals.post_delete.connect(self.handle_delete, sender=model)
+        for model in (Resource, Category, Keyword, Person):  # All models
             signals.post_save.connect(self.handle_related, sender=model)
             signals.post_delete.connect(self.handle_related, sender=model)
         signals.m2m_changed.connect(self.handle_m2m)
 
     def teardown(self):
-        for model in (Resource, Category, Keyword, Person):
+        for model in (Resource, Keyword, Person):
             signals.post_save.disconnect(self.handle_save, sender=model)
             signals.post_delete.disconnect(self.handle_delete, sender=model)
+        for model in (Resource, Category, Keyword, Person):
             signals.post_save.disconnect(self.handle_related, sender=model)
             signals.post_delete.disconnect(self.handle_related, sender=model)
         signals.m2m_changed.disconnect(self.handle_m2m)
@@ -27,6 +33,7 @@ class SignalProcessor(BaseSignalProcessor):
         Given an individual model instance, determine which backends the
         update should be sent to & update the object on those backends.
         """
+        logger.info('Handle save sent by %r for %s', sender, instance)
         using_backends = self.connection_router.for_write(instance=instance)
         for using in using_backends:
             index = self.connections[using].get_unified_index().get_index(sender)
@@ -37,6 +44,7 @@ class SignalProcessor(BaseSignalProcessor):
         Given an individual model instance, determine which backends the
         delete should be sent to & delete the object on those backends.
         """
+        logger.info('Handle delete sent by %r for %s', sender, instance)
         using_backends = self.connection_router.for_write(instance=instance)
         for using in using_backends:
             index = self.connections[using].get_unified_index().get_index(sender)
@@ -48,6 +56,7 @@ class SignalProcessor(BaseSignalProcessor):
         update should be sent to & update the object on those backends.
         """
         using_backends = self.connection_router.for_write(instance=instance)
+        logger.info('Handle related sent by %r for %s', sender, instance)
         for using in using_backends:
             unified_index = self.connections[using].get_unified_index()
             if sender == Resource:
@@ -66,6 +75,7 @@ class SignalProcessor(BaseSignalProcessor):
                     index.update_object(obj, using=using)
 
     def handle_m2m(self, sender, instance, action, reverse, model, **kwargs):
+        logger.info('Handle m2m sent by %r for %s with %s on %s (%s)', sender, instance, action, model, reverse)
         if action.startswith('post_') and Resource in (instance._meta.model, model):
             if instance._meta.model == Resource:
                 self.handle_save(instance._meta.model, instance)
