b156d374e4d29d6c77f818c21302a7472a5eb0c8
Denis Drescher <drescher@ferret-go.com>
2017-01-19 21:44:27 +0100

    Lower logging level of Haystack signal processing

--- researchlibrary/api/signal_processors.py
+++ researchlibrary/api/signal_processors.py
@@ -33,7 +33,7 @@ class SignalProcessor(BaseSignalProcessor):
         Given an individual model instance, determine which backends the
         update should be sent to & update the object on those backends.
         """
-        logger.info('Handle save sent by %r for %s', sender, instance)
+        logger.debug('Handle save sent by %r for %s', sender, instance)
         using_backends = self.connection_router.for_write(instance=instance)
         for using in using_backends:
             index = self.connections[using].get_unified_index().get_index(sender)
@@ -44,7 +44,7 @@ class SignalProcessor(BaseSignalProcessor):
         Given an individual model instance, determine which backends the
         delete should be sent to & delete the object on those backends.
         """
-        logger.info('Handle delete sent by %r for %s', sender, instance)
+        logger.debug('Handle delete sent by %r for %s', sender, instance)
         using_backends = self.connection_router.for_write(instance=instance)
         for using in using_backends:
             index = self.connections[using].get_unified_index().get_index(sender)
@@ -56,7 +56,7 @@ class SignalProcessor(BaseSignalProcessor):
         update should be sent to & update the object on those backends.
         """
         using_backends = self.connection_router.for_write(instance=instance)
-        logger.info('Handle related sent by %r for %s', sender, instance)
+        logger.debug('Handle related sent by %r for %s', sender, instance)
         for using in using_backends:
             unified_index = self.connections[using].get_unified_index()
             if sender == Resource:
@@ -75,7 +75,7 @@ class SignalProcessor(BaseSignalProcessor):
                     index.update_object(obj, using=using)
 
     def handle_m2m(self, sender, instance, action, reverse, model, **kwargs):
-        logger.info('Handle m2m sent by %r for %s with %s on %s (%s)', sender, instance, action, model, reverse)
+        logger.debug('Handle m2m sent by %r for %s with %s on %s (%s)', sender, instance, action, model, reverse)
         if action.startswith('post_') and Resource in (instance._meta.model, model):
             if instance._meta.model == Resource:
                 self.handle_save(instance._meta.model, instance)
